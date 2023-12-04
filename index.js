397290aa71aea1ef990ae634227c527008231a27
chala <chala@sreyas-mbp.wireless.newschool.edu>
2023-12-04 17:02:04 -0500

    second commit

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// Example code to fetch an animal fact
-document.getElementById('fetchButton').addEventListener('click', async () => {
-  const name = 'cat'; // You can replace this with user input or any other method to get the animal name
+// // Example code to fetch an animal fact
+// document.getElementById('fetchButton').addEventListener('click', async () => {
+//   const name = 'cat';
   
-  try {
-    const response = await fetch(`https://your-render-subdomain.onrender.com/animal/${name}`);
-    const data = await response.json();
+//   try {
+//     const response = await fetch(`https://your-render-subdomain.onrender.com/animal/${name}`);
+//     const data = await response.json();
     
-    // Display the fetched animal fact
-    alert(`Fact about ${data.name}: ${data.fact}`);
-  } catch (error) {
-    console.error(error);
-    // Handle errors if any
-    alert('Failed to fetch animal fact. Please try again.');
-  }
-});
+//     // Display the fetched animal fact
+//     alert(`Fact about ${data.name}: ${data.fact}`);
+//   } catch (error) {
+//     console.error(error);
+//     // Handle errors if any
+//     alert('Failed to fetch animal fact. Please try again.');
+//   }
+// });
 
 const express = require('express');
 const axios = require('axios');
@@ -24,16 +24,44 @@ const port = process.env.PORT || 3000;
 app.get('/animal/:name', async (req, res) => {
   const { name } = req.params;
 
-  try {
-    // Make a request to an external API (for example, 'https://some-animal-facts-api.com')
-    const response = await axios.get(`https://some-animal-facts-api.com/${name}`);
-    const animalFact = response.data.fact;
-
-    res.json({ name, fact: animalFact });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+  if (name == "cow"){
+    res.send("moo")
   }
+
+  if (name == "cat"){
+    res.send("meow")
+  }
+
+  if (name == "dog"){
+    res.send("woof")
+  }
+
+  if (name == "lion"){
+    res.send("roar")
+  }
+
+  if (name == "bird"){
+    res.send("chirp")
+  }
+
+  if (name == "bee"){
+    res.send("buzz")
+  }
+
+  if (name == "elephant"){
+    res.send("trumpets")
+  }
+
+  // try {
+    
+  //   const response = await axios.get(`https://api.api-ninjas.com/v1/animals?name=`);
+  //   const animalFact = response.data.fact;
+
+  //   res.json({ name, fact: animalFact });
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).json({ error: 'Internal Server Error' });
+  // }
 });
 
 app.listen(port, () => {
